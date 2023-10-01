239dba64e9c600ae264fc19a8b9209e8dec736fa
Amar Yadav <amaryadav071101@gmail.com>
2023-10-01 13:41:19 +0530

    updated in development branch

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
-This file contains the deployment code
-modified this file
-<<<<<<< HEAD
 updating this file newly
-<<<<<<< HEAD
-This line we can use for testing
-=======
-updating this line
->>>>>>> parent of e081974 (updated last  deploy.sh)
-=======
+>>>>>>> parent of 735a67e (added newfiles1)
 >>>>>>> parent of 735a67e (added newfiles1)
 
