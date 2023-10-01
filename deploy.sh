9b3efb8c8b708260848e166f5f034e8e388f3c43
Amar Yadav <amaryadav071101@gmail.com>
2023-10-01 10:46:57 +0530

    10:46

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 This file contains the deployment code
 modified this file
+<<<<<<< HEAD
 updating this file newly
 This line we can use for testing
+=======
+updating this line
+>>>>>>> parent of e081974 (updated last  deploy.sh)
 
